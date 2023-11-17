328e308a60fd278ae6042779af6e1cd6394343e5
kangminbeom <alsqja6945@naver.com>
2023-11-17 23:56:22 +0900

    commit

---
+++ src/main/java/com/hanghae99/goodssite/GoodsSiteApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae99.goodssite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae99/goodssite/GoodsSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae99.goodssite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
